c0a077f790dc9e14b3317e116d4bc42f0ccbf927
Andrey <OxCom@users.noreply.github.com>
2020-01-10 11:24:05 +0100

    Feature luhn (#3)
    * luhn constraint
    
    * luhn constraint

---
+++ src/Constraints/Luhn.js
@@ -0,0 +1,71 @@
+import AbstractConstraint from './AbstractConstraint';
+import { trim }           from '../Utils/functions';
+
+const MESSAGE_INVALID = 'Invalid card number.';
+
+export default class Luhn extends AbstractConstraint {
+    /**
+     * @param {{message: string, trim: boolean}} [options]
+     */
+    constructor(options) {
+        super(options);
+    }
+
+    /**
+     * @return {{message: string, trim: boolean}}
+     */
+    getDefaultOptions() {
+        return {
+            'trim': true,
+            'message': MESSAGE_INVALID,
+        };
+    }
+
+    /**
+     * Test provided value and return Error if occurs
+     *
+     * @param value
+     *
+     * @return {Error|undefined}
+     */
+    validate(value) {
+        if (this.options.trim) {
+            value = trim(value);
+        }
+
+        if (this.isEmptyValue(value)) {
+            return;
+        }
+
+        if (/[^0-9-\s]+/.test(value)) {
+            return this
+                .getViolationBuilder()
+                .setParameter('value', value)
+                .build(this.options.message);
+        }
+
+        let canonical = value.toString().replace(/\D/g, '');
+        let checkSum = 0;
+
+        for (let i = canonical.length - 1; i >= 0; i -= 2) {
+            checkSum += parseInt(canonical.charAt(i));
+        }
+
+        for (let i = canonical.length - 2; i >= 0; i -= 2) {
+            // checkSum += array_sum(str_split((int) $value[$i] * 2));
+            checkSum += (parseInt(canonical.charAt(i)) * 2)
+                .toString()
+                .split('')
+                .reduce((a, b) => {
+                    return parseInt(a) + parseInt(b);
+                }, 0);
+        }
+
+        if (0 === checkSum || 0 !== checkSum % 10) {
+            return this
+                .getViolationBuilder()
+                .setParameter('value', value)
+                .build(this.options.message);
+        }
+    }
+}
