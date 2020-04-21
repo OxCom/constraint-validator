50a76826e7242ae2e8f16e6ab573129bca4f6d7f
oxcom <gm.oxcom@gmail.com>
2020-04-21 18:56:07 +0200

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,16 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonJS from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
+// "main": "dist/how-long-till-lunch.cjs.js",
+//     "module": "dist/how-long-till-lunch.esm.js",
+//     "browser": "dist/how-long-till-lunch.umd.js",
+
 export default {
     input: './src/validator.js',
     output: [
-        {
-            file: './dist/constraint-validator.js',
-            format: 'umd',
-            name: 'constraint-validator',
-            plugins: [ terser() ]
-        }
+        { file: './dist/constraint-validator.umd.js', format: 'umd', plugins: [ terser() ], name: 'constraint-validator' },
+        { file: './dist/constraint-validator.cjs.js', format: 'cjs', plugins: [ terser() ] },
+        { file: './dist/constraint-validator.esm.js', format: 'es', plugins: [ terser() ] },
     ],
     plugins: [
         resolve(),
