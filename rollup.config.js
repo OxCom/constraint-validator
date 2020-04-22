8575295f72c19c0160c919ca86bb4f07880d4853
oxcom <gm.oxcom@gmail.com>
2020-04-22 07:59:49 +0200

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import {terser} from 'rollup-plugin-terser';
-import resolve from 'rollup-plugin-node-resolve';
-import commonJS from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonJS from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default {
